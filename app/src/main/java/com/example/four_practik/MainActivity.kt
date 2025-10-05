6a92f81215b9318be1fca9530deb3556134bc595
gopa-rgb <kulkaem@mer.ci.nsu.ru>
2025-10-05 14:02:46 +0700

    pochti final

---
+++ app/src/main/java/com/example/four_practik/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.four_practik
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.four_practik.ui.theme.Four_practikTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Four_practikTheme {
+                IppotecaApp()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/four_practik/ui/Start_Screen.kt
@@ -0,0 +1,33 @@
+package com.example.four_practik.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun StartScreen(
+    onStartOrderButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button (
+            onClick = onStartOrderButtonClicked,
+            Modifier.widthIn(min = 250.dp)
+        ) {
+            Text("Начать копить!")
+        }
+
+    }
+}
+
